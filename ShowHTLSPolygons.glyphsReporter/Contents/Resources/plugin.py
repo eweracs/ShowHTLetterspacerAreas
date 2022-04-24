1fe7fc35f426a72067e0d33a50a48c16dd864a25
Sebastian Carewe <sebastian.carewe@gmail.com>
2022-04-24 15:56:36 +0100

    Eliminate redundant refreshes

--- ShowHTLSPolygons.glyphsReporter/Contents/Resources/plugin.py
+++ ShowHTLSPolygons.glyphsReporter/Contents/Resources/plugin.py
@@ -9,13 +9,20 @@ from AppKit import NSAffineTransform
 
 import os
 import imp
-imp.load_source("HT_LetterSpacer_script", os.path.expanduser(GSGlyphsInfo.applicationSupportPath() + "/Repositories/HT "
-                                                             "Letterspacer/HT_LetterSpacer_script.py"))
+imp.load_source("HT_LetterSpacer_script",
+                os.path.expanduser(GSGlyphsInfo.applicationSupportPath() +
+                                   "/Repositories/HT Letterspacer/HT_LetterSpacer_script.py"))
 
 from HT_LetterSpacer_script import *
 
 
 class ShowHTLSPolygons(ReporterPlugin):
+
+	@objc.python_method
+	def start(self):
+		self.master_params = {}
+		self.glyphs_last_change = {}
+
 
 	@objc.python_method
 	def settings(self):
@@ -30,7 +37,7 @@ class ShowHTLSPolygons(ReporterPlugin):
 		xHeight = Glyphs.font.selectedFontMaster.xHeight
 		transform = NSAffineTransform.new()
 		slant = math.tan(Glyphs.font.selectedFontMaster.italicAngle * math.pi / 180.0)
-		transform.shearXBy_atCenter_(slant, xHeight/2)
+		transform.shearXBy_atCenter_(slant, xHeight / 2)
 	
 		for path in layer.paths:
 			for node in path.nodes:
@@ -40,44 +47,69 @@ class ShowHTLSPolygons(ReporterPlugin):
 
 	@objc.python_method
 	def create_polygons(self, layer):
-		engine = HTLetterspacerLib()
-		engine.paramDepth = int(Glyphs.font.selectedFontMaster.customParameters["paramDepth"] or 15)
-		engine.paramOver = int(Glyphs.font.selectedFontMaster.customParameters["paramOver"] or 0)
-		engine.xHeight = Glyphs.font.selectedFontMaster.xHeight
-		engine.factor = 1.0
-		engine.angle = Glyphs.font.selectedFontMaster.italicAngle
-		engine.upm = Glyphs.font.upm
-		engine.LSB = True
-		engine.RSB = True
-		engine.newWidth = False
-		engine.output = ""
+		if layer.master.id not in self.master_params:
+			self.master_params[layer.master.id] = {
+				"paramArea": layer.master.customParameters["paramArea"],
+				"paramDepth": layer.master.customParameters["paramDepth"],
+				"paramOver": layer.master.customParameters["paramOver"],
+			}
+		params_changed = False
+		for param in ["paramArea", "paramDepth", "paramOver"]:
+			if self.master_params[layer.master.id][param] != layer.master.customParameters[param]:
+				params_changed = True
 
-		new_layer = GSLayer()
+		if self.glyphs_last_change[layer.parent] != layer.parent.lastChange or not layer.tempData["polygons"] or \
+				params_changed:
+			print("hey")
+			engine = HTLetterspacerLib()
+			engine.paramDepth = int(Glyphs.font.selectedFontMaster.customParameters["paramDepth"] or 15)
+			engine.paramOver = int(Glyphs.font.selectedFontMaster.customParameters["paramOver"] or 0)
+			engine.xHeight = Glyphs.font.selectedFontMaster.xHeight
+			engine.factor = 1.0
+			engine.angle = Glyphs.font.selectedFontMaster.italicAngle
+			engine.upm = Glyphs.font.upm
+			engine.LSB = True
+			engine.RSB = True
+			engine.newWidth = False
+			engine.output = ""
 
-		decomposed_layer = layer.copyDecomposedLayer()
-		decomposed_layer.parent = layer.parent
+			new_layer = GSLayer()
 
-		for polygon in engine.setSpace(decomposed_layer, layer):
-			path = GSPath()
-			for node in polygon:
-				new_node = GSNode()
-				new_node.type = GSLINE
-				new_node.position = (node[0], node[1])
-				path.nodes.append(new_node)
-			path.closed = True
-			new_layer.paths.append(path)
+			decomposed_layer = layer.copyDecomposedLayer()
+			decomposed_layer.parent = layer.parent
 
-		new_layer = self.slant_layer(new_layer)
-		new_layer.completeBezierPath.fill()
+			for polygon in engine.setSpace(decomposed_layer, layer):
+				path = GSPath()
+				for node in polygon:
+					new_node = GSNode()
+					new_node.type = GSLINE
+					new_node.position = (node[0], node[1])
+					path.nodes.append(new_node)
+				path.closed = True
+				new_layer.paths.append(path)
+
+			new_layer = self.slant_layer(new_layer)
+			NSColor.greenColor().colorWithAlphaComponent_(0.4).set()
+			new_layer.completeBezierPath.fill()
+
+		else:
+			new_layer = layer.tempData["polygons"]
+			NSColor.greenColor().colorWithAlphaComponent_(0.4).set()
+			new_layer.completeBezierPath.fill()
+
+		layer.tempData["polygons"] = new_layer
 
 	@objc.python_method
 	def foreground(self, layer):
-		NSColor.greenColor().colorWithAlphaComponent_(0.4).set()
+		if layer.parent not in self.glyphs_last_change:
+			self.glyphs_last_change[layer.parent] = layer.parent.lastChange
 		self.create_polygons(layer)
+
 
 	@objc.python_method
 	def inactiveLayerForeground(self, layer):
-		NSColor.greenColor().colorWithAlphaComponent_(0.4).set()
+		if layer.parent not in self.glyphs_last_change:
+			self.glyphs_last_change[layer.parent] = layer.parent.lastChange
 		self.create_polygons(layer)
 
 	@objc.python_method
